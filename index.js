2ded826a3535db17fa8e996818eb534b7af8ca2f
Jhone <jhonealeixo@gmail.com>
2021-11-19 23:54:14 -0300

    Praticando herança

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente('João', 99944433309);
-
 const cliente2 = new Cliente('Jessica', 88844455503);
+const cliente3 = new Cliente('Emanoel', 9944553322);
 
 const contaCorrente1 = new ContaCorrente(cliente1, 1000, 10212-2, 0);
-
 const contaCorrente2 = new ContaCorrente(cliente2, 2000, 20214-2, 0);
+const contaPoupanca = new ContaPoupanca(100, cliente3, 3000, 20265-7);
 
-contaCorrente1.deposito(1000);
 
-contaCorrente1.transferencia(200, contaCorrente2);
-
-console.log(contaCorrente1);
+console.log(contaPoupanca);
