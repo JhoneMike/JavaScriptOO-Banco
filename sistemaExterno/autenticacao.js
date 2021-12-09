f609fcc5acd0f06266da3282d95fc5608a796630
Jhone <jhonealeixo@gmail.com>
2021-12-09 13:01:02 -0300

    Finalizando a class Autenticacao

--- sistemaExterno/autenticacao.js
+++ sistemaExterno/autenticacao.js
@@ -1,5 +1,12 @@
 export class Autenticacao{
-    logar(autenticavel, senha){
-        
+    static logar(autenticavel, senha){
+        if(Autenticacao.usuarioAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static usuarioAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
